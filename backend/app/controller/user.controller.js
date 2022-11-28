236bb7b0d4e65ae29b45b70dfba763b87ba264a2
Burcev Daniil <rayman01@bk.ru>
2022-11-28 11:05:33 +0800

    123

--- backend/app/controller/user.controller.js
+++ backend/app/controller/user.controller.js
@@ -11,3 +11,62 @@ exports.findAll = (req, res) => {
             globalFunctions.sendError(res, err);
         })
 };
+exports.create = (req, res) => {
+    User.create({
+        name: req.body.name,
+        username: req.body.username,
+        password: req.body.password
+    }).then(object => {
+        globalFunctions.sendResult(res, object);
+    }).catch(err => {
+        globalFunctions.sendError(res, err);
+    })
+};
+exports.update = (req, res) => {
+    User.update({
+            name: req.body.name,
+            username: req.body.username,
+            password: req.body.password
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    ).then(object => {
+        globalFunctions.sendResult(res, object);
+    }).catch(err => {
+        globalFunctions.sendError(res, err);
+    })
+};
+exports.delete = (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(() => {
+        globalFunctions.sendResult(res, 'Запись удалена');
+    }).catch(err => {
+        globalFunctions.sendError(res, err);
+    });
+};
+exports.findById = (req, res) => {
+    User.findByPk(req.params.id)
+        .then(object => {
+            globalFunctions.sendResult(res, object);
+        })
+        .catch(err => {
+            globalFunctions.sendError(res, err);
+        })
+};
+exports.findByUsername = (req, res) => {
+    User.findAll({
+        where: {
+            username: req.params.username
+        }
+    }).then(objects => {
+        globalFunctions.sendResult(res, objects);
+    }).catch(err => {
+        globalFunctions.sendError(res, err);
+    })
+};
--- backend/app/route/user.route.js
+++ backend/app/route/user.route.js
@@ -4,4 +4,18 @@ module.exports = (app) => {
     
     // Получение всех пользователей
     app.get('/api/users', user.findAll);
+    // Добавление пользователя
+app.post('/api/addUser', user.create);
+
+// Обновление данных пользователя по id
+app.post('/api/updateUser/:id', user.update);
+
+// Удаление данных пользователя по id
+app.post('/api/deleteUser/:id', user.delete);
+
+// Получение пользователя по id
+app.get('/api/user/:id', user.findById);
+
+// Получение пользователя по username
+app.get('/api/user/username/:username', user.findByUsername);
 };
