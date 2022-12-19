6f23c2132765276ab32a78de4dce5a788da14e34
Glomm0 <103648212+Glomm0@users.noreply.github.com>
2022-12-19 17:54:58 +0800

    done

--- frontend/src/components/authorization/Profile.vue
+++ frontend/src/components/authorization/Profile.vue
@@ -2,41 +2,52 @@
     <div class="container">
         <header>
             <h3>
-                Профиль <strong>{{currentUser.name}}</strong>
+                Профиль <strong>{{ currentUser.name }}</strong>
             </h3>
         </header>
         <p>
             <strong>Логин:</strong>
-            {{currentUser.username}}
+            {{ currentUser.username }}
         </p>
+        <button class="btn btn-danger" v-on:click="deleteUser">Удалить аккаунт</button>
     </div>
-    
 </template>
 <script>
-//import http from "../../http-common";
-    export default {
-        name: 'ProfileUser',
-        data() { // данные компонента (определение переменных)
-            return {
-                tasks: [],
-                displayContent: false // по умолчанию скрываем контент
-            };
-        },
-        computed: {
-            currentUser() {
-                //console.log(this.$store.state.auth.user)
-                return this.$store.state.auth.user;
-            }
-        },
-        methods: { // методы компонента
-            
-            
-        },
-        mounted() {
-            if (!this.currentUser) {
-                this.$router.push('/login');
-            }
-            
+import http from "../../http-common";
+export default {
+    
+    name: 'ProfileUser',
+    data() {
+        return {
+            tasks: [],
+            displayContent: false
+        };
+    },
+    computed: {
+        currentUser() {
+
+            return this.$store.state.auth.user;
         }
-    };
+    },
+    mounted() {
+        if (!this.currentUser) {
+            this.$router.push('/login');
+        }
+
+    },
+    methods:{
+        deleteUser() {
+            http
+                .post("/deleteUser/" +this.$store.state.auth.user.id)
+                .then(data=>{
+                    this.$store.dispatch('auth/logout');
+                    window.location.href = '/login';
+                    console.log(data)
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        }
+    }
+};
 </script>
